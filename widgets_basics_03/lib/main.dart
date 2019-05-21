034a9a99a9015be390d1413def44bd83b6eaf72f
KhanhND <khanhnd@KhanhNDs-iMac.local>
2019-05-21 22:41:19 +0700

    dart features

---
+++ widgets_basics_03/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+        accentColor: Colors.greenAccent,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
+          
+        ),
+        body: ProductManager('Food Tester'),
+      ),
+    );
+  }
+}
