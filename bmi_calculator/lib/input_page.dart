24db53bdc6cdaf46409b0d3ab89a99e66f2f4c93
Nguyen Duy Khanh <>
2019-05-27 17:13:09 +0700

    add button gesture

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'resuable_card.dart';
 
 const bottomContainerHeight = 80.0;
+const inactiveColor = Color(0xFF1111328);
 const activeCardColor = Color(0xFF101E33);
 const bottomContainerColor = Color(0xFFEB1555);
+
+  enum Gender {
+    male,
+    female,
+  }
 
 class InputPage extends StatefulWidget {
   @override
@@ -11,6 +19,10 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,13 +36,23 @@ class _InputPageState extends State<InputPage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    colour:activeCardColor,
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    colour:selectedGender == Gender.male ? activeCardColor : inactiveColor,
                     cardChild: IconContent(gender: 'MALE', icon: FontAwesomeIcons.mars),
                     ),
                   ),
                 Expanded(
                   child: ReusableCard(
-                    colour:activeCardColor,
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    colour: selectedGender == Gender.female ? activeCardColor : inactiveColor,
                     cardChild: IconContent(gender: 'FEMALE', icon: FontAwesomeIcons.venus),
                     ),
                 ),
@@ -62,53 +84,6 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class IconContent extends StatelessWidget {
 
-  IconContent({this.gender, this.icon});
 
-  final String gender;
-  final IconData icon;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon (
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          gender,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF808E98),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-
-  ReusableCard({@required this.colour, this.cardChild});
-
-  final Color colour;
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0), 
-        ),
-      );
-  }
-}
