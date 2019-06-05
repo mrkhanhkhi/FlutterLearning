dd45fb2ff4397a8cca6e5091c770b965bc8d624b
Nguyen Duy Khanh <>
2019-06-05 16:53:08 +0700

    add JSON parser

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,11 +13,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // TODO: implement initState
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getLocation();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+      "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22");
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
