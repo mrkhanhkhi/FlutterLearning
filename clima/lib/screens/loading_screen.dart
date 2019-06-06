ee093e8dc028ae0b8517ff1bd026007206bc44ad
KhanhND <khanhnd@KhanhNDs-iMac.local>
2019-06-06 22:35:16 +0700

    get actual weather data

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,7 +1,13 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
+import 'package:clima/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = '2155b483608a597586c18d076203d363';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,40 +15,46 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  @override void initState() {
-    // TODO: implement initState
+
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getLocation();
-  }
 
-  void getData() async {
-    http.Response response = await http.get(
-      "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22");
-    if (response.statusCode == 200) {
-      String data = response.body;
-    } else {
-      print(response.statusCode);
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper("https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
+
+    var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-            //Get the current location
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
         ),
       ),
     );
   }
 }
+//  RaisedButton(
+//           onPressed: () {
+//             getLocationData();
+//             //Get the current location
+//           },
--- clima/lib/services/location.dart
+++ clima/lib/services/location.dart
@@ -4,16 +4,15 @@ class Location {
   double latitude;
   double longitude;
 
-  Future <void> getLocation() async {
+  Future<void> getLocation() async {
     try {
-      Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
       print(position);
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
       print(e);
     }
-
   }
-
 }
