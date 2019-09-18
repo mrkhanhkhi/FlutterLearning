fae02caad7f7cfc7e4c6e837a8d8c6170bc3d3f9
Nguyen Duy Khanh <>
2019-09-18 17:09:36 +0700

    Chap 23 - 20

--- news/lib/src/app.dart
+++ news/lib/src/app.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
 import 'screens/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   // const App({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
+    return CommentsProvider(
+      child: StoriesProvider(
       child: MaterialApp(
         title: 'News',
         home: NewsList(),
         onGenerateRoute: routes
       ),
+    ),
     );
   }
 
@@ -25,7 +28,13 @@ class App extends StatelessWidget {
       );
     } else {
       return MaterialPageRoute(builder: (context) {
-        return NewsDetail();
+        final itemId = int.parse(settings.name.replaceFirst('/', ''));
+        final commentBloc = CommentsProvider.of(context);
+        commentBloc.fetchItemWithComments(itemId);
+        
+        return NewsDetail(
+          itemId: itemId
+        );
         }
       );
     }
--- news/lib/src/screens/news_detail.dart
+++ news/lib/src/screens/news_detail.dart
@@ -1,15 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/comments_bloc.dart';
+import '../blocs/comments_provider.dart';
+import '../model/item_model.dart';
+import 'dart:async';
 
 class NewsDetail extends StatelessWidget {
-  const NewsDetail({Key key}) : super(key: key);
+  final int itemId;
+  
+  NewsDetail({this.itemId});
 
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('Im a news detail'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading');
+        }
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context , AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!snapshot.hasData) {
+           return Text('Loading');
+        }
+        return buildtitle(itemSnapshot.data);
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildtitle(ItemModel item) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      alignment: Alignment.topCenter,
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold
+        ),
+      ),
     );
   }
 }
