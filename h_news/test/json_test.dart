3c39ea5b4c2913138163e96ddb74131caf7a7315
KhanhND <>
2019-10-23 21:53:35 +0700

    add json test url

--- h_news/test/json_test.dart
+++ h_news/test/json_test.dart
@@ -1,5 +1,7 @@
 import 'package:h_news/json_parsing.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   test("parses topstories.json", () {
@@ -13,5 +15,14 @@ void main() {
     const jsonString =
         """{"by":"dhouston","descendants":71,"id":8863,"kids":[9224,8952,8917,8884,8887,8943,8869,8940,8908,8958,9005,8873,9671,9067,9055,8865,8881,8872,8955,10403,8903,8928,9125,8998,8901,8902,8907,8894,8870,8878,8980,8934,8876],"score":104,"time":1175714200,"title":"My YC app: Dropbox - Throw away your USB drive","type":"story","url":"http://www.getdropbox.com/u/2/screencast.html"}""";
     expect(parseArticle(jsonString).by, "dhouston");
+  });
+
+  test("parses item.json over a network", () async {
+    final url = 'https://hacker-news.firebaseio.com/v0/newstories.json';
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final idListJson = json.decode(response.body);
+          expect(parseArticle(response.body).by, "dhouston");
+    }
   });
 }
