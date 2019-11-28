e42348df39450c516b706af5de5b2af724d53d77
Nguyen Duy Khanh <>
2019-11-28 17:08:12 +0700

    add single video model

--- youtube_search/lib/data/model/serializer/serializers.dart
+++ youtube_search/lib/data/model/serializer/serializers.dart
@@ -1,12 +1,16 @@
 library serializers;
 
 import 'dart:convert';
-
+import 'package:youtube_search/data/model/common/model_common.dart';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
+import 'package:youtube_search/data/model/detail/video_item.dart';
+import 'package:youtube_search/data/model/detail/video_snippet.dart';
+import 'package:youtube_search/data/model/detail/youtube_video_response.dart';
 import 'package:youtube_search/data/model/search/modeal_search.dart';
+import 'package:youtube_search/data/model/detail/model_detail.dart';
 
 part 'serializers.g.dart';
 
@@ -17,6 +21,9 @@ part 'serializers.g.dart';
   SearchSnippet,
   Thumbnails,
   Thumbnail,
+  YoutubeVideoResponse,
+  VideoItem,
+  VideoSnippet,
 ])
 final Serializers serializers =
     (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
